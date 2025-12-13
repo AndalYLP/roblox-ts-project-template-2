02165b7f54876774c2f50bb5b7cbeff6559a1ab1
AndalYLP <160054201+AndalYLP@users.noreply.github.com>
2025-12-13 17:12:58 -0500

    add tag enum

---
+++ src/types/enums/tag.d.ts
@@ -0,0 +1,10 @@
+/**
+ * ### Enum that represents different tags used for identifying parts in the game.
+ *
+ * These tags are assigned to specific parts of the player's character or other
+ * objects.
+ */
+export declare const enum Tag {
+	PlayerCharacter = "PlayerCharacter",
+	PlayerHead = "PlayerHead",
+}
