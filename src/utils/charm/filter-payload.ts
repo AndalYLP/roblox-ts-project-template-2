f5703ba96ba42eb5129a39dae776675eea10fd94
AndalYLP <160054201+AndalYLP@users.noreply.github.com>
2025-12-13 18:25:47 -0500

    add charm utils

---
+++ src/utils/charm/filter-payload.ts
@@ -0,0 +1,33 @@
+import { SyncPayload } from "@rbxts/charm-sync";
+import { GlobalAtoms } from "shared/store/sync/atoms";
+
+/**
+ * Filters the payload to only include the player's data.
+ *
+ * @param player The player to send the payload to.
+ * @param payload The payload to filter.
+ * @returns A new payload that only includes the player's data.
+ */
+export function filterPayload(player: Player, payload: SyncPayload<GlobalAtoms>) {
+	if (payload.type === "init") {
+		return {
+			...payload,
+			data: {
+				...payload.data,
+				"datastore/players": {
+					[player.Name]: payload.data["datastore/players"][player.Name],
+				},
+			},
+		};
+	}
+
+	return {
+		...payload,
+		data: {
+			...payload.data,
+			"datastore/players": payload.data["datastore/players"] && {
+				[player.Name]: payload.data["datastore/players"][player.Name],
+			},
+		},
+	};
+}
