4e9aef1acee792b13a986a15e6f94a8fec537fd1
Andal <andal089.az@gmail.com>
2026-01-24 22:36:37 -0500

    add ordered data store

--- src/server/services/player/data/index.ts
+++ src/server/services/player/data/index.ts
@@ -2,7 +2,10 @@ import { Service } from "@flamework/core";
 import { effect } from "@rbxts/charm";
 import { createCollection, setConfig } from "@rbxts/lapis";
 import DataStoreServiceMock from "@rbxts/lapis-mockdatastore";
-import { Players } from "@rbxts/services";
+import { DataStoreService, Players } from "@rbxts/services";
+import { setInterval } from "@rbxts/set-timeout";
+import { t } from "@rbxts/t";
+import type { OnInit } from "@flamework/core";
 import type { Collection, Document } from "@rbxts/lapis";
 import type { Logger } from "@rbxts/log";
 
@@ -15,7 +18,25 @@ import {
 	setPlayerData,
 } from "shared/store/atoms/player/datastore";
 import { KickCode } from "types/enums/kick-reason";
+import type { OnPlayerJoin, OnPlayerLeave } from "server/services/player";
+import type { PlayerEntity } from "server/services/player/entity";
 import type { PlayerRemovalService } from "server/services/player/removal";
+
+interface OrderedDataEntry {
+	name: OrderedData;
+	playerDataKey?: NestedKeyOf<PlayerData>;
+}
+
+interface OrderedDataConfig {
+	ascending?: boolean;
+	maxValue?: number;
+	minValue?: number;
+	pageSize?: number;
+}
+
+type OrderedData = "Money" | "Test";
+
+const ORDERED_DATA_UPDATE_TIME = 5 * 60;
 
 /**
  * Service for loading and saving player data. This service is responsible for
@@ -23,8 +44,10 @@ import type { PlayerRemovalService } from "server/services/player/removal";
  * changes to the player's data document in the data store.
  */
 @Service()
-export class PlayerDataService {
+export class PlayerDataService implements OnInit, OnPlayerJoin, OnPlayerLeave {
 	private readonly collection: Collection<PlayerData>;
+	private readonly dataStoreName = IS_STUDIO ? "development" : "production";
+	private readonly orderedDataStores: OrderedDataEntry[] = [];
 
 	constructor(
 		private readonly logger: Logger,
@@ -36,10 +59,23 @@ export class PlayerDataService {
 			});
 		}
 
-		this.collection = createCollection<PlayerData>(IS_STUDIO ? "development" : "production", {
+		this.collection = createCollection<PlayerData>(this.dataStoreName, {
 			defaultData: defaultPlayerData,
 			validate,
 		});
+	}
+	public onInit(): Promise<void> | void {
+		this.registerDataStore("Money", "balance.money");
+	}
+
+	public getPlayerOrderedData(
+		orderedDatastore: OrderedData,
+		{ ascending = false, maxValue, minValue, pageSize = 100 }: OrderedDataConfig,
+	): DataStorePages {
+		return DataStoreService.GetOrderedDataStore(
+			this.dataStoreName,
+			orderedDatastore,
+		).GetSortedAsync(ascending, pageSize, minValue, maxValue);
 	}
 
 	/**
@@ -77,6 +113,69 @@ export class PlayerDataService {
 		} catch (err) {
 			this.logger.Warn(`Failed to load data for ${player.UserId}: ${err}`);
 			this.playerRemovalService.removeForBug(player, KickCode.PlayerProfileUndefined);
+		}
+	}
+
+	public onPlayerJoin(playerEntity: PlayerEntity): void {
+		playerEntity.janitor.Add(
+			setInterval(() => {
+				this.savePlayerOrderedData(playerEntity);
+			}, ORDERED_DATA_UPDATE_TIME),
+		);
+	}
+
+	public onPlayerLeave(playerEntity: PlayerEntity): Promise<void> | void {
+		this.savePlayerOrderedData(playerEntity);
+	}
+
+	private getPlayerData(playerData: PlayerData, nestedKey: NestedKeyOf<PlayerData>): number {
+		const keys = nestedKey.split(".");
+		let value = playerData;
+		for (const key of keys) {
+			value = value[key as never];
+		}
+
+		assert(t.number(value), `Value is not a number: ${value}`);
+
+		return value;
+	}
+
+	private registerDataStore(
+		dataStoreName: OrderedData,
+		playerDataKey?: NestedKeyOf<PlayerData>,
+	): void {
+		assert(
+			this.orderedDataStores.find((entry) => entry.name === dataStoreName) === undefined,
+			`Datastore provided already exists.`,
+		);
+
+		this.orderedDataStores.push({
+			name: dataStoreName,
+			playerDataKey: playerDataKey,
+		});
+
+		this.logger.Info(`Registered ordered data ${dataStoreName}`);
+	}
+
+	private savePlayerOrderedData({ UserId }: PlayerEntity): void {
+		const playerData = getPlayerData(UserId);
+		if (!playerData) {
+			return;
+		}
+
+		for (const entry of this.orderedDataStores) {
+			if (entry.playerDataKey === undefined) {
+				continue;
+			}
+
+			try {
+				DataStoreService.GetOrderedDataStore(
+					IS_STUDIO ? "Development" : "Production",
+					entry.name,
+				).SetAsync(UserId, this.getPlayerData(playerData, entry.playerDataKey));
+			} catch (err) {
+				this.logger.Warn(`Failed to save ordered data ${entry.name} for ${UserId}: ${err}`);
+			}
 		}
 	}
 }
