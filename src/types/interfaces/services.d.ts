bb077f63d0003cac6777d830b85781639912994b
Andal <andal089.az@gmail.com>
2026-01-27 12:20:20 -0500

    update jest integration and type imports in runtime files

--- src/types/interfaces/services.d.ts
+++ src/types/interfaces/services.d.ts
@@ -1,17 +1,51 @@
-interface ServerScriptService {
-	TS: {
-		centurion: {
-			commands: Folder;
-			guards: Folder;
-			run: ModuleScript;
+import type { runCLI } from "@rbxts/jest";
+
+import type { config } from "test/jest.config";
+
+declare global {
+	interface ServerScriptService {
+		TS: {
+			__test__: Folder | undefined;
+			centurion: {
+				commands: Folder;
+				guards: Folder;
+				run: ModuleScript;
+			} & Folder;
 		} & Folder;
-	} & Folder;
+	}
+
+	interface ReplicatedStorage {
+		rbxts_include: {
+			node_modules: {
+				"@rbxts": {
+					jest: {
+						src: {
+							Name: "JEST/SRC";
+						} & ModuleScript;
+					} & Folder;
+					WaitForChild(
+						childName: "jest",
+					): ReplicatedStorage["rbxts_include"]["node_modules"]["@rbxts"]["jest"];
+				} & Folder;
+			} & Folder;
+		} & Folder;
+		TS: {
+			__test__: Folder | undefined;
+			centurion: {
+				types: Folder;
+			} & Folder;
+		} & Folder;
+		WaitForChild(childName: "rbxts_include"): ReplicatedStorage["rbxts_include"];
+	}
+
+	interface TestService {
+		"jest.config": { Name: "jest.config" } & ModuleScript;
+	}
+
+	function require(moduleScript: TestService["jest.config"]): { config: typeof config };
+	function require(
+		moduleScript: ReplicatedStorage["rbxts_include"]["node_modules"]["@rbxts"]["jest"]["src"],
+	): { runCLI: typeof runCLI };
 }
 
-interface ReplicatedStorage {
-	TS: {
-		centurion: {
-			types: Folder;
-		} & Folder;
-	} & Folder;
-}
+export {};
--- test/runtime.server.ts
+++ test/runtime.server.ts
@@ -1,26 +1,15 @@
-import type JestModule from "@rbxts/jest";
-
-import type * as ConfigModule from "test/jest.config";
-
 const ReplicatedStorage = game.GetService("ReplicatedStorage");
 const ServerScriptService = game.GetService("ServerScriptService");
 const TestService = game.GetService("TestService");
-
-const { config } = require(
-	TestService.FindFirstChild("jest.config") as ModuleScript,
-) as typeof ConfigModule;
-
-const jestLua = ReplicatedStorage.WaitForChild("rbxts_include")
-	.FindFirstChild("node_modules")!
-	.FindFirstChild("@rbxts")!
-	.WaitForChild("JestLua");
 const { runCLI } = require(
-	(jestLua as { Jest: ModuleScript } & Instance).Jest,
-) as typeof JestModule;
+	ReplicatedStorage.WaitForChild("rbxts_include").node_modules["@rbxts"].WaitForChild("jest").src,
+);
+
+const { config } = require(TestService["jest.config"]);
 
 const [success, result] = runCLI(script, config, [
-	ServerScriptService.FindFirstChild("TS")!.FindFirstChild("__test__")!,
-	ReplicatedStorage.FindFirstChild("TS")!.FindFirstChild("__test__")!,
+	ServerScriptService.TS.__test__!,
+	ReplicatedStorage.TS.__test__!,
 ]).await();
 
 if (!success) {
