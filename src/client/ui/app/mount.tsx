195efc03f53517316211c429873109c08809765a
Andal <andal089.az@gmail.com>
2026-03-02 14:31:04 -0500

    use PLAYER_GUI directly from const file

--- src/client/ui/app/mount.tsx
+++ src/client/ui/app/mount.tsx
@@ -1,8 +1,8 @@
 import Vide, { mount } from "@rbxts/vide";
 
-import { LOCAL_PLAYER } from "client/constants/player";
+import { PLAYER_GUI } from "client/constants/player";
 import { App } from "client/ui/app";
 
 export async function mountApp(): Promise<void> {
-	mount(() => <App />, LOCAL_PLAYER.WaitForChild("PlayerGui"));
+	mount(() => <App />, PLAYER_GUI);
 }
