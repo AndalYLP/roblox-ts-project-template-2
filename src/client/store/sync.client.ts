3e1b4fcb7e682328cc86b966f43cba5388789fbf
Andal <andal089.az@gmail.com>
2026-07-16 01:30:52 -0500

    Correctly pass atoms to client.addSignals

--- src/client/store/sync.client.ts
+++ src/client/store/sync.client.ts
@@ -3,7 +3,7 @@ import { client } from "@rbxts/charm-sync";
 import { events } from "client/network";
 import { atoms } from "shared/store/sync/atoms";
 
-client.addSignals({ atoms });
+client.addSignals(atoms);
 
 events.store.sync.connect((payload) => {
 	client.patch(payload);
