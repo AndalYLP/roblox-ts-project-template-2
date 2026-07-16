16558b75252daef898454a0744b6df3939d2ea2b
Andal <andal089.az@gmail.com>
2026-07-16 11:23:32 -0500

    Mount Notifications and Settings components in the player GUI

--- src/client/ui/app/mount.tsx
+++ src/client/ui/app/mount.tsx
@@ -2,7 +2,11 @@ import Vide, { mount } from "@rbxts/vide";
 
 import { PLAYER_GUI } from "client/constants/player";
 import { App } from "client/ui/app";
+import { Notifications } from "client/ui/notifications/notifications";
+import { Settings } from "client/ui/settings/settings";
 
 export async function mountApp(): Promise<void> {
 	mount(() => <App />, PLAYER_GUI);
+	mount(() => <Notifications />, PLAYER_GUI);
+	mount(() => <Settings />, PLAYER_GUI);
 }
